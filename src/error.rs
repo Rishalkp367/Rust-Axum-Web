c567e76dab39caf4a4e8f8ac96947ec87b7261fa
rishalkp367 <rishalkp367@gmail.com>
2025-10-24 23:46:42 +0530

    `Added error handling and authentication middleware, updated dependencies and models`

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,21 @@
 use axum::{ http::{ StatusCode }, response::{ IntoResponse, Response } };
+use serde::Serialize;
 
 pub type Result<T> = std::result::Result<T, Error>;
-#[derive(Debug)]
+#[derive(Clone, Debug, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
 
+    //Auth
     AuthFailNoTokenCookie,
-    
+    AuthFailTokenNoFormat,
+    AuthFailCtxNotInRequestExt,
+
+    //Model
     TicketDeleteFailIdNotFound {
         id: u64,
-    }, 
+    },
 }
 
 //Error Boilerplate
@@ -17,6 +23,40 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - ERROR - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED CLIENT ERROR").into_response()
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        response.extensions_mut().insert(self);
+
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            // -- Auth
+            | Self::AuthFailNoTokenCookie
+            | Self::AuthFailTokenNoFormat
+            | Self::AuthFailCtxNotInRequestExt => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+
+            // Model
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // -- Fallback
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, ClientError::SERVICE_ERROR),
+        }
+    }
+}
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
